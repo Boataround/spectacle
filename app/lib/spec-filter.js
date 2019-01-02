cf5fbe7b26e75c6855358f0a226bf18992bc33ec
Miroslav Kostka <miroslav.kostka1988@gmail.com>
2019-01-02 16:46:05 +0100

    CHANGE: spectacle-hide property remove refferenced parameters from documentation

--- app/lib/spec-filter.js
+++ app/lib/spec-filter.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const { jsonSearch } = require('./json-reference');
+const { jsonSearch, resolveLocal } = require('./json-reference');
 
 const METHODS = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch'];
 
@@ -13,8 +13,24 @@ module.exports = function specFilter(options, specData) {
             });
         }
 
+	    let refs = findRefs(specCopy, specCopy.paths);
+
+	    if(specCopy.definitions) {
+		    specCopy.definitions = _.pickBy(specCopy.definitions, function(def, defName) {
+			    return refs.has(`#/definitions/${defName}`) && !def['x-spectacle-hide'];
+		    });
+	    }
+
+
+	    if(specCopy.parameters) {
+		    specCopy.parameters = _.pickBy(specCopy.parameters, function(param, paramName) {
+			    return refs.has(`#/parameters/${paramName}`) && !param['x-spectacle-hide'];
+		    });
+	    }
+
         if(specCopy.paths) {
             specCopy.paths = _.pickBy(specCopy.paths, function(path) {
+
                 if(path['x-spectacle-hide']) {
                     return false;
                 }
@@ -22,6 +38,28 @@ module.exports = function specFilter(options, specData) {
                 for(let method of METHODS) {
                     if(path[method] && path[method]['x-spectacle-hide']) {
                         delete path[method];
+                        continue;
+                    }
+                    if(path[method]){
+                        if(path[method].hasOwnProperty('parameters'))
+                        {
+	                        path[method]['parameters'] = _.filter(path[method]['parameters'], function(parameter) {
+		                            if(parameter.hasOwnProperty('$ref')){
+                                        const parametersRegex = /^(?:#\/parameters\/)(.*)$/;
+                                        const definitionsRegex = /^(?:#\/definitions\/)(.*)$/;
+                                        let m;
+
+                                        if ((m = parametersRegex.exec(parameter.$ref)) !== null) {
+                                            return specCopy.parameters.hasOwnProperty(m[1]);
+                                        }
+                                        if ((m = definitionsRegex.exec(parameter.$ref)) !== null) {
+                                            return specCopy.definitions.hasOwnProperty(m[1]);
+                                        }
+                                        return false;
+                                    }
+		                        return true;
+	                        });
+                        }
                     }
                 }
 
@@ -30,19 +68,7 @@ module.exports = function specFilter(options, specData) {
                 });
             });
 
-            let refs = findRefs(specCopy, specCopy.paths);
 
-            if(specCopy.definitions) {
-                specCopy.definitions = _.pickBy(specCopy.definitions, function(def, defName) {
-                    return refs.has(`#/definitions/${defName}`);
-                });
-            }
-
-            if(specCopy.parameters) {
-                specCopy.parameters = _.pickBy(specCopy.parameters, function(param, paramName) {
-                    return refs.has(`#/parameters/${paramName}`);
-                });
-            }
         }
     }
 
